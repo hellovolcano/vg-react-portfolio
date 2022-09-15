889e27d6045bd9b9eb26eeeb4f006855f5a3310e
hellovolcano <valerie.gleason@gmail.com>
2022-09-14 21:52:07 -0500

    adding material ui drawer for mobile nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Nav from '../Nav'
+import MobileNav from '../MobileNav'
 
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 600)
+    
+    const updateWindow = () => {
+        setIsMobile(window.innerWidth < 700)
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWindow)
+        return () => window.removeEventListener('resize', updateWindow)
+    })
 
     return(
         <header>
-            <h1>Valerie Gleason <span class="mobile-heading">| Full-Stack Developer</span></h1>
-            <Nav />
+                <h1>Valerie Gleason <span className="mobile-heading">| Full-Stack Developer</span></h1>
+                {isMobile ? (
+                    <MobileNav /> ) : (<Nav />)
+                }
+                
+            
         </header>
     )
 }
