ab12c7fb6cad8b6792c01b6a96dc4928aa030dab
hellovolcano <valerie.gleason@gmail.com>
2022-09-16 08:58:16 -0500

    added validation to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import SendIcon from '@mui/icons-material/Send'
 import Stack from '@mui/material/Stack'
+import Alert from '@mui/material/Alert'
 import { GlobalStyles } from '@mui/material'
+import { validateEmail} from '../../utils/helpers'
 
 const ContactForm = () => {
+    const [formState, setFormState] = useState({name: '', email: '', message: ''})
+    const [errorMessage, setErrorMessage] = useState('')
+    const { name, email, message } = formState
+
+    const handleChange = (e) => {
+        if (e.target.name === 'email') {
+            console.log('email clicked')
+            const isValid = validateEmail(e.target.value)
+            if (!isValid) {
+                setErrorMessage('Your email is invalid')
+            } else {
+                setErrorMessage('')
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required`)
+            } else {
+                setErrorMessage('')
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formState)
+    }
+
     return(
         <section>
 
@@ -13,16 +46,19 @@ const ContactForm = () => {
 
             <h2>Contact Me</h2>
             <Stack 
-            component="form" 
-            spacing={2} 
-            sx={{
-                width: 4/5,
-                mx: "auto"
-        }}>
-                <TextField id="name" label="Name" variant="outlined" />
-                <TextField id="email" label="Email" variant="outlined" />
-                <TextField id="message" label="Message" variant="outlined" multiline rows={4} />
-                <Button variant="outlined">
+                component="form" 
+                spacing={2} 
+                sx={{
+                    width: 4/5,
+                    mx: "auto"
+                }}
+                
+            >
+                <TextField id="name" name="name" label="Name" variant="outlined" onBlur={handleChange} defaultValue={name} />
+                <TextField id="email" name="email" label="Email" variant="outlined" onBlur={handleChange} defaultValue={email} />
+                <TextField id="message" name="message" label="Message" variant="outlined" onBlur={handleChange} defaultValue={message} multiline rows={4} />
+                {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+                <Button variant="outlined" onClick={handleSubmit}>
                     Submit  &nbsp;&nbsp;<SendIcon />
                 </Button>
         </Stack>
