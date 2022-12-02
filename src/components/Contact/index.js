840f76b512908fcaf0e7ed42e07dd0f46708faf6
hellovolcano <valerie.gleason@gmail.com>
2022-12-02 11:06:15 -0600

    adding formspree for forms

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,35 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { useForm } from '@formspree/react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import SendIcon from '@mui/icons-material/Send'
 import Stack from '@mui/material/Stack'
 import Alert from '@mui/material/Alert'
-import { validateEmail} from '../../utils/helpers'
+import { validateEmail } from '../../utils/helpers'
+
+const FollowUp = (
+    <section className="contact">
+        <h3>Thanks for reaching out!</h3>
+        <Stack
+        spacing={2} 
+        sx={{
+            mx: "auto",
+        }}>
+        I will try to respond to your message in 24-48 hours.
+    </Stack>
+    </section>
+    
+)
 
 const ContactForm = () => {
     const [formState, setFormState] = useState({name: '', email: '', message: ''})
     const [errorMessage, setErrorMessage] = useState('')
     const { name, email, message } = formState
+
+    const [state, handleSubmit] = useForm("mjvzdaro")
+    if (state.succeeded) {
+        return FollowUp
+    }
 
     const handleChange = (e) => {
         if (e.target.name === 'email') {
@@ -32,11 +52,6 @@ const ContactForm = () => {
         }
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(formState)
-    }
-
     return(
         <section className="contact">
 
@@ -48,13 +63,14 @@ const ContactForm = () => {
                     width: 4/5,
                     mx: "auto",
                 }}
+                onSubmit={handleSubmit}
                 
             >
                 <TextField  sx={{ color: 'antiquewhite'}} id="name" name="name" label="Name" variant="outlined" onBlur={handleChange} defaultValue={name} />
                 <TextField  sx={{ fontFamily: 'Poppins'}} id="email" name="email" label="Email" variant="outlined" onBlur={handleChange} defaultValue={email} />
                 <TextField  id="message" name="message" label="Message" variant="outlined" onBlur={handleChange} defaultValue={message} multiline rows={4} sx={{ fontFamily: 'Poppins'}} />
                 {errorMessage && <Alert severity="error" variant="filled" sx={{ fontFamily: 'Poppins'}} >{errorMessage}</Alert>}
-                <Button variant="outlined" onClick={handleSubmit} >
+                <Button variant="outlined" type="submit" disabled={state.submitting}>
                     Submit  &nbsp;&nbsp;<SendIcon />
                 </Button>
         </Stack>
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='vg-react-portfolio'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
