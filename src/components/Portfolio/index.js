30852ad93febd3f21b18645205090e5cc9957d19
Valerie <5265744+hellovolcano@users.noreply.github.com>
2026-02-05 16:32:22 -0600

    fix demonstrates bug

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,15 +8,15 @@ const Portfolio = () => {
             internalLink: 'hippohonk',
             externalLink: 'https://github.com/hellovolcano/hippohonk',
             description: 'Hippohonk is dedicated to finding and rating showcasing bands at SXSW Austin. Originally created with Ruby on Rails in 2011 and ported to PERN.',
-            technologies: ['Postgres','Node.js','React.js','Material-UI','JavaScript','HTML','CSS', 'Express.js'],
+            demonstrates: ['Postgres','Node','React','Material-UI','JavaScript','HTML','CSS', 'Express'],
             imgName: 'hippohonk.png'
         },
         {
             name: 'Find A Movie',
             internalLink: 'find-a-movie',
             externalLink: 'https://find-a-movie-5e830608c35b.herokuapp.com/',
-            description: 'Quickly search for the movie you want to watch amonst multiple popular streaming services.',
-            technologies: ['HTML', 'CSS', 'JavaScript'],
+            description: 'Originally created as a group project for a Full Stack Coding camp and redone to improve design and UX.',
+            demonstrates: ['React', 'CSS', 'JavaScript', 'Node', 'Express', 'REST APIs'],
             imgName: 'iStreamuStream.png'
         },
         {
@@ -24,7 +24,7 @@ const Portfolio = () => {
             externalLink: 'https://www.our1907house.com',
             internalLink: 'our-1907-house',
             description: 'Frontend only website for hosting information about a century home.',
-            technologies: ['HTML', 'CSS','JavaScript','Node.js','Handlebars','MySQL'],
+            demonstrates: ['React'],
             imgName: 'mvc-blog.png'
         },
     ])
